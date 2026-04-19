330ef6e6b69f165e526087983f0d0e3f976ceab5
PaDIshAX <kirsem797@gmail.com>
2026-04-19 16:01:04 +0300

    Первая реализация интерактивного режима. Возможности: help, list, start, delete.

--- launcher.cpp
+++ launcher.cpp
@@ -10,93 +10,149 @@
 using json = nlohmann::json;
 
 
-std::vector<std::string> options={"list","start <preset name>", "delete <preset name>", "add <preset name>"};
+std::vector<std::string> options={
+	"'list' for all presets",
+	"'start' for launching presets",
+	"'delete' for deleting presets",
+	"'exit' for exit" 
+};
 
 
-int main(int argc, char* argv[]){
-	if(argc < 2){
-		std::cerr<<"Using: "<<argv[0]<<" <option>"<<" [preset name]\n";
-		std::cerr<<"Use: help\n";
-		return 1;
+void saving(json& presets){
+	std::ofstream file("presets.json");
+	file<<presets.dump(4);
+}
+
+
+void list_presets(const json& presets){
+	std::cout<<"All presets:\n";
+	for(const auto& [name,command]:presets.items()){
+		std::cout<<"Preset ["<<name<<"] contains "<<command.size()<<" commands:\n";
+		for(const auto& cmd:command){
+			std::cout<<"	- "<<cmd<<'\n';
+		}
+	}
+}
+
+
+void start(std::string preset, const json& presets){
+	if(preset == "@#" || !presets.contains(preset)){
+		while(true){
+			std::cout<<"Please, enter preset: ";
+			std::cin>>preset;
+
+			if(preset == "list"){
+				list_presets(presets);
+				continue;
+			}
+
+			else if(preset == "exit"){
+				std::cin.ignore(10000,'\n');
+				return;
+			}
+
+			if(!presets.contains(preset)){
+				std::cerr<<"Error: preset is not found! Use 'list' for all presets or 'exit' for exit\n";
+			}
+			else break;
+		}
+		std::cin.ignore(10000,'\n');
 	}
 
+	std::cout<<"Starting preset ["<<preset<<"]:\n";
+	for(const auto& app:presets[preset]){
+		std::string command=app;
+		std::string full_command=command+" > /dev/null 2>&1 &";
+
+		std::cout<<"	-> "<<command<<'\n';
+
+		int status=std::system(full_command.c_str());
+		if(status != 0){
+			std::cout<<"Process ["<<command<<"] ended with status "<<status;
+		}
+	}
+	std::cout<<"Done!\n";
+}
+
+
+void Delete(std::string preset, json& presets){
+	if(preset == "@#" || !presets.contains(preset)){
+		while(true){
+			std::cout<<"Please, enter preset: ";
+			std::cin>>preset;
+
+			if(preset == "list"){ 
+				list_presets(presets);
+				continue;
+			}
+
+			else if(preset == "exit"){
+				std::cin.ignore(10000,'\n');
+				return;
+			}
+
+			if(!presets.contains(preset)){
+				std::cerr<<"Error: preset is not found! Use 'list' for all presets or 'exit' for exit\n";
+			}
+			else break;
+		}
+		std::cin.ignore(10000,'\n');
+	}
+	presets.erase(preset);
+	saving(presets);
+}
+
+
+int main(){
 	std::ifstream file("presets.json");
-	if(!file.is_open()){
-		std::cerr<<"Error: file is not opened\n";
-		return 1;
-	}
 	json presets;
-	file>>presets;
-
-	std::string option=argv[1];
-
-	if(option  == "start"){
-		if(argc < 3){
-			std::cerr<<"Using: "<<argv[0]<<" start [preset name]\n";
-			return 1;
-		}
-		std::string preset_name=argv[2];
-
-		if(presets.contains(preset_name)){
-			std::cout<<"Launching preset "<<preset_name<<'\n';
-			int cur_return;
-			for(const auto& app: presets[preset_name]){
-				std::string command=app;
-				std::string full_command=command+" > /dev/null 2>&1 &";
-				std::cout<<"Procces: "<<command<<'\n';
-	
-				cur_return=std::system(full_command.c_str());
-				if(cur_return != 0){
-					std::cerr<<"Command "<<command<<" returned with status "<<cur_return<<"!\n";
-				}
-			}
-			std::cout<<"Ready!\n";
-		}
-		else{
-			std::cerr<<"Error: preset "<<preset_name<<" not found\n";
-			return 1;
-		}
+	if(file.is_open()){
+		file>>presets;
+		file.close();
 	}
 
-	else if(option == "list"){
-		std::cout<<"Avaliable presets:\n";
-		for(const auto& [name,command]:presets.items()){
-			std::cout<<"* "<<name<<" ("<<command.size()<<") apps\n";
-			for(const auto& cmd:command){
-				std::cout<<"	-"<<cmd<<'\n';
+	std::string line;
+	std::cout<<"Welcome! Type 'help' for commands\n";
+
+	while(true){
+		std::cout<<"~ ";
+
+		if(!std::getline(std::cin, line) || line == "exit") break;
+		if(line.empty()) continue;
+
+		std::stringstream ss(line);
+		std::string cmd;
+		std::vector<std::string> argc;
+		while(ss >> cmd) 
+			argc.push_back(cmd);
+
+		std::string option=argc[0];
+
+		if(option == "help"){
+			std::cout<<"Avaliable options:\n";
+			for(const auto& method:options){
+				std::cout<<method<<'\n';
 			}
 		}
-	}
 
-	else if(option == "delete"){
-		if(argc < 3){
-			std::cerr<<"Use: delete [preset name]\n";
-			return 1;
+		else if(option == "list") list_presets(presets);
+		else if(option == "start"){
+			std::string parametr;
+			if(argc.size() > 1) parametr=argc[1];
+			else parametr="@#";
+
+			start(parametr, presets);
 		}
+		else if(option == "delete"){
+			std::string parametr;
+			if(argc.size() > 1) parametr=argc[1];
+			else parametr="@#";
 
-		std::string preset=argv[2];
-		if(!presets.contains(preset)){
-			std::cerr<<"Error: preset "<<preset<<" not found\n";
+			Delete(parametr, presets);
 		}
 		else{
-			presets.erase(preset);
-			std::ofstream file("presets.json");
-			file<<presets.dump(4);
-			std::cout<<"Preset "<<preset<<" deleted\n";
+			std::cout<<"Error: option is not correct!\n";
 		}
-
-	}
-
-	else if(option == "help"){
-		std::cout<<"Avaliable options:\n";
-		for(const auto& method: options){
-			std::cout<<method<<'\n';
-		}
-	}
-
-	else{
-		std::cerr<<"Unknown option\n";
-		return 1;
 	}
 	return 0;
 }
